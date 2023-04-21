20ff326dc57a4e6c095abb6f4a4d5844ca0f0941
ShutovKS <i@shutovks.ru>
2023-04-21 12:07:58 +0300

    Added initial prototype locations and settings

---
+++ Assets/Internal assets/Scripts/XR/Hand/HandAnimatorController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Internal_assets.Scripts.XR.Hand
+{
+    public class HandAnimatorController : MonoBehaviour
+    {
+        private Animator _animator;
+        [SerializeField] private InputActionProperty _grabAction;
+        [SerializeField] private InputActionProperty _activateAction;
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            _animator.SetFloat("Grip", _grabAction.action.ReadValue<float>());
+            _animator.SetFloat("Trigger", _activateAction.action.ReadValue<float>());
+        }
+    }
+}
