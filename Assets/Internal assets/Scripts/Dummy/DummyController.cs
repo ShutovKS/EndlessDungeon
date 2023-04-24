34edfa62cb0e19859fde6f402896c5357749b2f3
ShutovKS <i@shutovks.ru>
2023-04-24 14:03:27 +0300

    Fixed bugs and real-time mesh generation on the training dummy

--- Assets/Internal assets/Scripts/Dummy/DummyController.cs
+++ Assets/Internal assets/Scripts/Dummy/DummyController.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using Item;
 using UnityEngine;
@@ -7,14 +6,14 @@ namespace Dummy
 {
     public class DummyController : MonoBehaviour
     {
-        private static Animator _animator;
-        private static BoxCollider _boxCollider;
-        private static MeshCollider _meshCollider;
         private static readonly int Damage = Animator.StringToHash("Damage");
         private static readonly int Dead = Animator.StringToHash("Dead");
         private static readonly int Reset = Animator.StringToHash("Reset");
-        private static int _health = 100;
-        private static bool _isDead = false;
+
+        private Animator _animator;
+
+        private int _health = 1000;
+        private bool _isAction = false;
 
         private int Health
         {
@@ -23,7 +22,6 @@ namespace Dummy
             {
                 _health = value;
                 if (_health > 0) return;
-                _isDead = true;
                 _animator.SetTrigger(Dead);
                 StartCoroutine(Restart());
             }
@@ -31,41 +29,25 @@ namespace Dummy
 
         private void Start()
         {
-            if (_animator == null && TryGetComponent<Animator>(out var animator))
-                _animator = animator;
-            if (_boxCollider == null && TryGetComponent<BoxCollider>(out var boxCollider))
-                _boxCollider = boxCollider;
-            if (_meshCollider == null && TryGetComponent<MeshCollider>(out var meshCollider))
-                _meshCollider = meshCollider;
+            _animator = GetComponent<Animator>();
         }
 
         private void OnCollisionEnter(Collision other)
         {
-            if (!_isDead && other.gameObject.TryGetComponent<ItemDamage>(out var itemDamage))
-            {
-                _animator.SetTrigger(Damage);
-                Health -= itemDamage.Damage;
-            }
+            if (_isAction || !other.gameObject.TryGetComponent<ItemDamage>(out var itemDamage)) return;
+            _animator.SetTrigger(Damage);
+            Health -= itemDamage.Damage;
         }
 
-        private static IEnumerator Restart()
+        //using animation
+        public void Action() => _isAction = true;
+        public void Idle() => _isAction = false;
+        public void Revived() => _health = 1000;
+
+        private IEnumerator Restart()
         {
-            yield return new WaitForSeconds(5);
+            yield return new WaitForSeconds(3.5f);
             _animator.SetTrigger(Reset);
-        }
-
-        public void Died()
-        {
-            _meshCollider.enabled = false;
-            _boxCollider.enabled = true;
-        }
-
-        public void Revived()
-        {
-            _health = 100;
-            _isDead = false;
-            _meshCollider.enabled = true;
-            _boxCollider.enabled = false;
         }
     }
 }
---
+++ Assets/Internal assets/Scripts/Dummy/DummyUpdateMesh.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dummy
+{
+    public class DummyUpdateMesh : MonoBehaviour
+    {
+        private Mesh _mesh;
+        private MeshFilter _meshFilter;
+        private MeshCollider _meshCollider;
+        private SkinnedMeshRenderer _skinnedMeshRenderer;
+
+        private Collider[] _childrenColliders;
+
+        private void Start()
+        {
+            _meshCollider = GetComponent<MeshCollider>();
+            _meshFilter = GetComponentInChildren<MeshFilter>();
+            _skinnedMeshRenderer = GetComponentInChildren<SkinnedMeshRenderer>();
+
+            _mesh = new Mesh();
+
+            StartCoroutine(UpdateNewMesh());
+
+            _childrenColliders = GetAllCollidersInChildren(transform);
+        }
+
+        //using animation
+        public void Action()
+        {
+            _meshCollider.enabled = false;
+            foreach (var collider in _childrenColliders)
+            {
+                collider.enabled = true;
+            }
+        }
+
+        public void Idle()
+        {
+            _meshCollider.enabled = true;
+            foreach (var collider in _childrenColliders)
+            {
+                collider.enabled = false;
+            }
+        }
+
+        private IEnumerator UpdateNewMesh()
+        {
+            _skinnedMeshRenderer.BakeMesh(_mesh);
+
+            _meshFilter.mesh = _mesh;
+            _meshFilter.mesh.RecalculateBounds();
+
+            yield return new WaitForSeconds(0.1f);
+
+            _meshCollider.sharedMesh = _mesh;
+            _meshCollider.sharedMesh.RecalculateBounds();
+        }
+
+        public Collider[] GetAllCollidersInChildren(Transform parentTransform)
+        {
+            List<Collider> colliders = new List<Collider>();
+
+            int childCount = parentTransform.childCount;
+            for (int i = 0; i < childCount; i++)
+            {
+                Transform childTransform = parentTransform.GetChild(i);
+
+                Collider[] childColliders = childTransform.GetComponents<Collider>();
+                if (childColliders != null && childColliders.Length > 0)
+                {
+                    colliders.AddRange(childColliders);
+                }
+
+                if (childTransform.childCount > 0)
+                {
+                    Collider[] childCollidersInChildren = GetAllCollidersInChildren(childTransform);
+
+                    if (childCollidersInChildren != null && childCollidersInChildren.Length > 0)
+                    {
+                        colliders.AddRange(childCollidersInChildren);
+                    }
+                }
+            }
+
+            return colliders.ToArray();
+        }
+    }
+}
