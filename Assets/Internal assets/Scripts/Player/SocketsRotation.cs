1ce7a5b767dfc249d740ab7e51d64b57afed1143
ShutovKS <i@shutovks.ru>
2023-04-27 14:24:59 +0300

    Configured sockets for weapons

---
+++ Assets/Internal assets/Scripts/Player/SocketsRotation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class SocketsRotation : MonoBehaviour
+    {
+        private Transform _triggerTransform;
+
+        private void Awake()
+        {
+            _triggerTransform = Camera.main!.transform;
+        }
+
+        private void Update()
+        {
+            var rotation = _triggerTransform.rotation;
+            transform.rotation = Quaternion.Euler(new Vector3(0, rotation.eulerAngles.y, 0));
+        }
+    }
+}
