512ec63c43ce39ef8cdb732043587456357bc10e
ShutovKS <schutow.kir@yandex.ru>
2023-05-22 14:57:23 +0300

    Added fire in hand if rotation vertical

---
+++ Assets/Internal assets/Scripts/Socket/SocketInPlayer/RotationForwardPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Socket.SocketInPlayer
+{
+    public class RotationForwardPlayer : MonoBehaviour
+    {
+        [SerializeField] private Transform _triggerTransform;
+
+        private void Update()
+        {
+            var rotation = _triggerTransform.rotation;
+            transform.rotation = Quaternion.Euler(new Vector3(0, rotation.eulerAngles.y, 0));
+        }
+    }
+}
---
+++ Assets/Internal assets/Scripts/Units/Player/FireInHand.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Units.Player
+{
+    public class FireInHand : MonoBehaviour
+    {
+        private GameObject _fire;
+        private Transform _transform;
+        private bool _state;
+
+        private void Start()
+        {
+            _transform = transform.parent;
+            _fire = transform.GetChild(0).gameObject;
+        }
+
+        private void Update()
+        {
+            var rotation = _transform.rotation.eulerAngles;
+            _state = rotation.z is > 75 and < 135 && rotation.x is > 330 or < 30;
+            _fire.SetActive(_state);
+        }
+    }
+}
